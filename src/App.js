0137bb872edc3de49134c0866618d49c8be9cc09
rPhase <25046850+rPhase@users.noreply.github.com>
2022-08-26 15:45:54 -0700

    setup basic UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import React, { Fragment } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+import { useEffect } from 'react';
 import './App.css';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
 
 const App = () => {
-  return <div className='App'>My App</div>;
+  useEffect(() => {
+    // Init Materialize JS
+    M.AutoInit();
+  }, []);
+
+  return (
+    <Fragment>
+      <SearchBar />
+      <div className='container'>
+        <AddBtn />
+        <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
+        <TechListModal />
+        <Logs />
+      </div>
+    </Fragment>
+  );
 };
 
 export default App;
